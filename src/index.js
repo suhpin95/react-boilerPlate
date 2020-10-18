b1b0c6344a0591ba69898a96208ff03cb70e9e2a
Suhas <suhpin95@gmail.com>
2020-10-18 10:48:55 +0530

    Added dependencies

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Welcome extends React.Component{
+    render(){
+        return(
+            `
+                <div>
+                    <h2> Configuring the React JS </h2>
+                </div>
+            `
+        )
+    }
+}
+
+ReactDOM.render( <Welcome/>, document.getElementById("app") )
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/**
+ * This would help us 
+ * help us to people the 
+ * code from the soures and
+ * set the destination
+ */
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: './dist',
+    },
+    module: {
+      rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+      ]
+    },
+  };
