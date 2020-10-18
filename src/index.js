7cd874754d285caed4356d2b71e86d4f2f4bf8b4
Suhas <suhpin95@gmail.com>
2020-10-18 11:34:53 +0530

    Added prettier and ESLint

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class Welcome extends React.Component{
-    render(){
-        return(
-                <div>
-                    <h2> Configuring the React JS </h2>
-                </div>
-        )
+class Welcome extends React.Component {
+    render () {
+        return (
+            <div>
+                <h2> Configuring the React JS </h2>
+            </div>
+        );
     }
 }
 
-ReactDOM.render( <Welcome/>, document.getElementById("app") )
+ReactDOM.render(<Welcome />, document.getElementById('app'));
