2423f48852fc5e5731fa07ae048e9efad57a1a1f
Suhas <suhpin95@gmail.com>
2020-10-18 11:08:51 +0530

    Basic Boiler Plate without CRA has been achieved

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import ReactDOM from "react-dom";
 class Welcome extends React.Component{
     render(){
         return(
-            `
                 <div>
                     <h2> Configuring the React JS </h2>
                 </div>
-            `
         )
     }
 }
